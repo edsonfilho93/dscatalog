32d6accc5489ab9c0781c436a3a5e0e2d9808367
Edson <edsonfilho734@gmail.com>
2021-05-25 00:41:54 -0300

    H2 Database

--- backend/src/main/java/com/edson/dscatalog/entities/Category.java
+++ backend/src/main/java/com/edson/dscatalog/entities/Category.java
@@ -2,14 +2,21 @@ package com.edson.dscatalog.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
-@AllArgsConstructor
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
     private static final long SerialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+
 }
