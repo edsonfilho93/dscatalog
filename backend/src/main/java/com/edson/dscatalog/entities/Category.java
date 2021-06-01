7e828e4bd8f16b9d493486dec30bc11bd4c1e424
Edson <edsonfilho734@gmail.com>
2021-06-01 00:09:13 -0300

    Auditing data

--- backend/src/main/java/com/edson/dscatalog/entities/Category.java
+++ backend/src/main/java/com/edson/dscatalog/entities/Category.java
@@ -1,13 +1,14 @@
 package com.edson.dscatalog.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Instant;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
@@ -18,4 +19,21 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    @Setter(AccessLevel.NONE)
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    @Setter(AccessLevel.NONE)
+    private Instant updateAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updateAt = Instant.now();
+    }
 }
