b04030d74f91dc5397ab6c0ea44435f5ecb6905e
Edson <edsonfilho734@gmail.com>
2021-06-03 20:30:48 -0300

    Removing Lombok from the project

--- backend/src/main/java/com/edson/dscatalog/entities/Product.java
+++ backend/src/main/java/com/edson/dscatalog/entities/Product.java
@@ -8,45 +8,32 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
-@NoArgsConstructor
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
-    private static final long SerialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
 
     @Column(columnDefinition = "TEXT")
     private String description;
-
     private Double price;
-
     private String imgUrl;
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant date;
 
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    @Setter(AccessLevel.NONE)
-    private Instant createdAt;
-
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    @Setter(AccessLevel.NONE)
-    private Instant updateAt;
-
     @ManyToMany
-    @JoinTable(
-            name = "tb_product_category",
+    @JoinTable(name = "tb_product_category",
             joinColumns = @JoinColumn(name = "product_id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id")
-    )
-    @Setter(AccessLevel.NONE)
-    private Set<Category> categories = new HashSet<>();
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    Set<Category> categories = new HashSet<>();
+
+    public Product() {
+    }
 
     public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
         this.id = id;
@@ -55,19 +42,82 @@ public class Product implements Serializable {
         this.price = price;
         this.imgUrl = imgUrl;
         this.date = date;
-
-        this.createdAt = Instant.now();
     }
 
-    @PrePersist
-    public void prePersist() {
-        createdAt = Instant.now();
+    public Long getId() {
+        return id;
     }
 
-    @PreUpdate
-    public void preUpdate() {
-        updateAt = Instant.now();
+    public void setId(Long id) {
+        this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 }
