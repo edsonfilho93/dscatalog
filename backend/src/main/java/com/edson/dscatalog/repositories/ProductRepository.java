ddc02fa420b781fcda1493393b3fbcea0ab10a13
Edson <edsonfilho734@gmail.com>
2021-07-19 23:38:57 -0300

    Product repository tests

--- backend/src/main/java/com/edson/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/edson/dscatalog/repositories/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.edson.dscatalog.repositories;
 
-import com.edson.dscatalog.entities.Category;
 import com.edson.dscatalog.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
---
+++ backend/src/test/java/com/edson/dscatalog/factories/Factory.java
@@ -0,0 +1,24 @@
+package com.edson.dscatalog.factories;
+
+import com.edson.dscatalog.entities.Category;
+import com.edson.dscatalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+    public static Product createProductWithIdNull() {
+        Product product = new Product(null, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-09-17T03:00:00Z"));
+        product.getCategories().add(createCategory());
+        return product;
+    }
+
+    public static Product createProduct() {
+        Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-09-17T03:00:00Z"));
+        product.getCategories().add(createCategory());
+        return product;
+    }
+
+    public static Category createCategory() {
+        return new Category(4L, "Eletronics");
+    }
+}
