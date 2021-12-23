16d7697e346ea45f9d09e56adfbff87f225c67f1
edson <edsonfilho734@gmail.com>
2021-12-23 13:24:49 -0300

    Created DTO, repository and service for Role and User

---
+++ backend/src/main/java/com/edson/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.edson.dscatalog.repositories;
+
+import com.edson.dscatalog.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ backend/src/main/java/com/edson/dscatalog/services/UserService.java
@@ -0,0 +1,91 @@
+package com.edson.dscatalog.services;
+
+import com.edson.dscatalog.dto.UserDTO;
+import com.edson.dscatalog.dto.UserInsertDTO;
+import com.edson.dscatalog.entities.Role;
+import com.edson.dscatalog.entities.User;
+import com.edson.dscatalog.repositories.RoleRepository;
+import com.edson.dscatalog.repositories.UserRepository;
+import com.edson.dscatalog.services.exceptions.DatabaseException;
+import com.edson.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<UserDTO> findAll(Pageable pageable) {
+        Page<User> list = repository.findAll(pageable);
+        return list.map(UserDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id) {
+        User entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Entidade não encontrada"));
+        return new UserDTO(entity);
+    }
+
+    @Transactional
+    public UserDTO insert(UserInsertDTO dto) {
+        User entity = new User();
+        copyDtoToEntity(entity, dto);
+        entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+        entity = repository.save(entity);
+        return new UserDTO(entity);
+
+    }
+
+    @Transactional
+    public UserDTO update(Long id, UserDTO dto) {
+        try {
+            User entity = repository.getOne(id);
+            copyDtoToEntity(entity, dto);
+            entity = repository.save(entity);
+            return new UserDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("id não encontrado:".concat(String.valueOf(id)));
+        } catch (DataIntegrityViolationException e2) {
+            throw new DatabaseException("Violação de Integridade do BD");
+        }
+    }
+
+    private void copyDtoToEntity(User entity, UserDTO dto) {
+        entity.setFirstName(dto.getFirstName());
+        entity.setLastName(dto.getLastName());
+        entity.setEmail(dto.getEmail());
+
+        entity.getRoles().clear();
+
+        dto.getRoles().forEach(roleDTO -> {
+            Role role = roleRepository.getOne(roleDTO.getId());
+            entity.getRoles().add(role);
+        });
+    }
+}
