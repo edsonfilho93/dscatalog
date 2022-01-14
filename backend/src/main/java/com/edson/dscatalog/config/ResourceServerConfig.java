d971d1b8a341628d19a93ba0a66b80791dd04c8b
edson <edsonfilho734@gmail.com>
2022-01-14 17:10:27 -0300

    Resource server

---
+++ backend/src/main/java/com/edson/dscatalog/config/ResourceServerConfig.java
@@ -0,0 +1,40 @@
+package com.edson.dscatalog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private static final String[] PUBLIC_ROUTES = {"/oauth/token"};
+
+    private static final String[] OPERATOR_OR_ADMIN_ROUTES = {"/products/**", "/categories/**"};
+
+    private static final String[] ADMIN_ROUTES = {"/users/**"};
+
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(PUBLIC_ROUTES).permitAll()
+                .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN_ROUTES).permitAll()
+                .antMatchers(OPERATOR_OR_ADMIN_ROUTES).hasAnyRole("OPERATOR", "ADMIN")
+                .antMatchers(ADMIN_ROUTES).hasRole("ADMIN")
+                .anyRequest().authenticated();
+    }
+}
